8a20569d975a1497575deb413eb12ca088b71647
kkoroleva <korolevakarinka22@gmail.com>
2021-01-23 23:55:11 +0300

    Fixes

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ const styles = () => {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
+    .pipe(gulp.dest("build/css"))
     .pipe(postcss([
       autoprefixer(),
       csso()
